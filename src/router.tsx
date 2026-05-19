b665ecb8ec38368ca02ba1667ecfe5b7a1d90a4a
Vilenan <vilenan@mail.ru>
2026-05-19 16:48:14 +0300

    add nested routes with app layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
+import AppLayout from './AppLayout';
 import TodoDetailsPage from './pages/TodoDetailsPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
-  },
-  {
-    path: '/todo/:id',
-    element: <TodoDetailsPage />,
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: 'todo/:id',
+        element: <TodoDetailsPage />,
+      },
+    ],
   },
 ]);
